badacb1152b28ccfcdeb11d1928716842401397c
Bethina S.M <bethina.s.moen@gmail.com>
2024-09-11 11:20:25 +0200

    Forslag til Drive function

---
+++ bethina.js
@@ -0,0 +1,21 @@
+
+
+function drive(){
+   let randomEvent = Math.floor(Math.Random()*5 + 1) 
+
+   if (randomEvent === 1){
+        // Kjører videre og ingenting skjer
+   }else if (randomEvent === 2){
+        // Du møter en kompis
+   }else if (randomEvent === 3){
+        // du møter på Karen
+   }else if (randomEvent === 4){
+        // du finner en ting!
+        let item = getRandomItem();
+   }
+}
+
+function getRandomItem() {
+    // array med ting fra Thomas
+    
+}
