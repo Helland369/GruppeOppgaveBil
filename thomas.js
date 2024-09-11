e0bebf7329f11efd1763115d52b938ebcecd2578
Thomas Helland <thomas.helland97@gmail.com>
2024-09-11 10:23:59 +0200

    array

---
+++ thomas.js
@@ -0,0 +1,15 @@
+let kul = ["Kul1", "Kul2", "Kul3", "Ikke kul1", "Ikke kul2", "Ikke kul3"];
+
+let hils = ["Yo", "Hallo", "Morn"];
+
+function hilsner(hei) {
+    if (hei == 1) {
+        hils[0];
+    }
+    if (hei == 2) {
+        hils[1];
+    }
+    if (hei == 3) {
+        hils[2];
+    }
+}
