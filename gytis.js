b9e025f2daae6e964801b8dd3aa279fd06e2357f
J0nWater <79053067+J0nWater@users.noreply.github.com>
2024-09-11 10:37:15 +0200

    Update gytis.js

--- gytis.js
+++ gytis.js
@@ -4,11 +4,11 @@ function updateView(){
     <button onclick="drive()">Kjør</button>
     <button onclick="pickUp()">Plukk opp</button>
     <button onclick="ignore()">Ignorer</button>
-
-    <div id="hilse">
-        <button onclick="hilse1">What's up</button>
-        <button onclick="hilse2">God dag</button>
-        <button onclick="hilse3">High five</button>
+    
+    <div id="hils">
+        <button onclick="hils1">What's up</button>
+        <button onclick="hils2">God dag</button>
+        <button onclick="hils3">High five</button>
     </div>
 
     `
