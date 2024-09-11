f05119c6285ef09cffae9bad91bbfc0207a429c1
Bethina S.M <bethina.s.moen@gmail.com>
2024-09-11 14:47:54 +0200

    update

--- bethina.js
+++ bethina.js
@@ -9,18 +9,15 @@ function drive() {
         driveText()
         // Kjører videre og ingenting skjer
     } else if (randomEvent === 2) {
-        
         meetingBro()
         // Du møter en kompis
         currentFriend = randomFriend();
         preferredGreeting = randomGreeting();
         
     } else if (randomEvent === 3) {
-        meetingKaren()
         // Du møter på Karen
     } else if (randomEvent === 4) {
         foundItemText()
-        
         // Du finner en ting!
         let item = getRandomItem();
     }
