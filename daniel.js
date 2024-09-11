912338d2dc06c75e207c5e5e05eb6e62ff49e39c
Thomas Helland <thomas.helland97@gmail.com>
2024-09-11 13:40:23 +0200

    qweqweqweqweqwe

--- daniel.js
+++ daniel.js
@@ -14,6 +14,7 @@ let textbox = [
 ];
 let value;
 
+<<<<<<< HEAD
  function driveText() {
       if (textbox == [0]) {
           main.innerHTML = textbox [0];
@@ -22,36 +23,46 @@ let value;
   }
  
 function ignoreText() {
+=======
+function driveText() {
+    if (textbox == [0]) {
+        main.innerHTML = textbox[0];
+    }
+    console.log(drive);
+}
+
+function ignore() {
+>>>>>>> b042b27 (qweqweqweqweqwe)
     if (textbox == [1]) {
-        main.innerHTML = textbox [1];
+        main.innerHTML = textbox[1];
     }
     console.log(ignore);
 }
 
 function pickUpText() {
     if (textbox == [2]) {
-        main.innerHTML = textbox [2];
+        main.innerHTML = textbox[2];
     }
     console.log(pickUp);
 }
 
 function hils1Text() {
     if (textbox == [3]) {
-        main.innerHTML = textbox [3];
+        main.innerHTML = textbox[3];
     }
     console.log(hils1);
 }
 
 function hils2Text() {
     if (textbox == [4]) {
-        main.innerHTML = textbox [4];
+        main.innerHTML = textbox[4];
     }
     console.log(hils2);
 }
 
 function hils3Text() {
     if (textbox == [5]) {
-        main.innerHTML = textbox [5];
+        main.innerHTML = textbox[5];
     }
     console.log(hils3);
 }
