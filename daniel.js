547ccd849daf3a34a465caaa19215d7ccdde2b76
DanielLarsen-spec <larsen97daniel@gmail.com>
2024-09-11 14:31:38 +0200

    Update daniel.js

--- daniel.js
+++ daniel.js
@@ -21,68 +21,68 @@ let value;
 
 function driveText() {
     if (textbox == [0]) {
-        main.innerHTML = textbox[0];
+        textB.innerHTML = textbox[0];
     }
     console.log(drive);
 }
 
 function ignoreText() {
     if (textbox == [1]) {
-        main.innerHTML = textbox[1];
+        textB.innerHTML = textbox[1];
     }
     console.log(ignore);
 }
 
 function pickUpText() {
     if (textbox == [2]) {
-        main.innerHTML = textbox[2];
+        textB.innerHTML = textbox[2];
     }
     console.log(pickUp);
 }
 
 function greetingText() {
     if (textbox == [3]) {
-        main.innerHTML = textbox[3];
+        textB.innerHTML = textbox[3];
     }
     console.log(hils1);
 }
 
 function highFiveText() {
     if (textbox == [4]) {
-        main.innerHTML = textbox[4];
+        textB.innerHTML = textbox[4];
     }
     console.log(hils2);
 }
 
 function foundItemText() {
     if (textbox == [5]) {
-        main.innerHTML = textbox[5];
+        textB.innerHTML = textbox[5];
     }
     console.log(hils3);
 }
 
 function meetingBro(){
 if(textbox == [6]){
-main.innerHTML = textbox[6];
+textB.innerHTML = textbox[6];
 }
 }
 
 function meetingKaren(){
 if(textbox == [7]){
-main.innerHTML = textbox[7];
+textB.innerHTML = textbox[7];
 }
 
 }
 
 function broHappy(){
 if(textbox == [8]){
-main.innerHTML = textbox[8];
+textB.innerHTML = textbox[8];
 }
 }
 
 function broMad(){
 if(textbox == [9]){
-main.innerHTML = textbox[9];
+textB.innerHTML = textbox[9];
 }
 
 }
