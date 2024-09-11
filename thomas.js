bb6d36bec3e9bf7553d50316d8fe7cdde104cc7c
Thomas Helland <thomas.helland97@gmail.com>
2024-09-11 10:33:55 +0200

    array

--- thomas.js
+++ thomas.js
@@ -1,15 +1,31 @@
-let kul = ["Kul1", "Kul2", "Kul3", "Ikke kul1", "Ikke kul2", "Ikke kul3"];
+let array = [
+    "Kul1",
+    "Kul2",
+    "Kul3",
+    "Ikke kul1",
+    "Ikke kul2",
+    "Ikke kul3",
+    "Yo",
+    "Hallo",
+    "Morn",
+];
 
 let hils = ["Yo", "Hallo", "Morn"];
 
+//hilsner(1);
 function hilsner(hei) {
     if (hei == 1) {
         hils[0];
+        console.log(hils);
     }
     if (hei == 2) {
         hils[1];
+        console.log(hils);
     }
     if (hei == 3) {
         hils[2];
+        console.log(hils);
     }
 }
+
+function randomKul() {}
