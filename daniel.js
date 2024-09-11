0424e5a0f752fba8e8416b6bafda28fed3811287
DanielLarsen-spec <larsen97daniel@gmail.com>
2024-09-11 12:22:45 +0200

    Update daniel.js

--- daniel.js
+++ daniel.js
@@ -11,59 +11,73 @@ let value = 0;
 
 
 function drive(){
-if(){
-    
+if(textbox == [0]){
+main.innerHTML =""; 
+console.log(drive)
 }
-
-
-
-
 
 }
 
 function ignore(){
-
-
-
-
-
-
-
+if(textbox == [1]){
+main.innerHTML = "";
 }
 
+}
 
 function pickUp(){
-
-
-
-
-
-
+if(textbox == [2]){
+    main.innerHTML = "";
 }
 
+}
 
 function hils1(){
-
-
-
+if(textbox == [3]){
+main.innerHTML = "";
+}
 }
 
-    
 function hils2(){
+if(textbox == [4]){
+    main.innerHTML = "";
 
+}
 
 
 
 }
 
 function hils3(){
-
-
-
-
-
+if(textbox == [5]){
+    main.innerHTML = "";
 
 }
+}
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
