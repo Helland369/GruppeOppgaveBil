3d3c7590adcf654298128f5cf185cf02610a848f
DanielLarsen-spec <larsen97daniel@gmail.com>
2024-09-11 14:33:05 +0200

    Update bethina.js

--- bethina.js
+++ bethina.js
@@ -9,31 +9,39 @@ function drive() {
         driveText()
         // Kjører videre og ingenting skjer
     } else if (randomEvent === 2) {
+        meetingBro()
+       
         // Du møter en kompis
         currentFriend = randomFriend();
         preferredGreeting = randomGreeting();
         
     } else if (randomEvent === 3) {
+        meetingKaren()
         // Du møter på Karen
     } else if (randomEvent === 4) {
+        foundItemText()
+        
         // Du finner en ting!
         let item = getRandomItem();
     }
-    console.log(drive);
 }
 
 function getRandomItem() {
 
     // array med ting fra Thomas
-   //return items[Math.floor(Math.random() * items.lenght)];
+   return items[Math.floor(Math.random() * items.lenght)];
 }
 
 
 function greetFriend (selectedGreeting) {
     if (selectedGreeting === preferredGreeting) {
-        // Kompisen din likte hilsen din! Du får 2 poeng og får kjøre videre
+        broHappy()
+       
+        // Kompisen din likte hilsen din! Du får kjøre videre
     }else {
-        // Kompisen din likte ikke hilsen din, minus 1 poeng!
+        broMad()
+       
+        // Kompisen din likte ikke hilsen din
     }
 }
 
